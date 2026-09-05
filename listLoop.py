38f987a3e8a528e870d924fe6c3165b2f7a4e6d8
Einstein123 <richardeinstein40@gmail.com>
2026-09-05 12:36:56 +0000

    Demonstrate the application of list comprehension in python

---
+++ listLoop.py
@@ -0,0 +1,16 @@
+#normal traditional for loop
+
+numbers = [1,2,3,4,5,6,7,8,9]
+
+squared_numbers = []
+for number in numbers:
+    number_squared = number ** 2
+    squared_numbers.append(number_squared)
+
+print(squared_numbers)
+
+
+#using list comprehension
+
+squared_numbers1 = [x **2 for x in range(1,10)]
+print(squared_numbers1)
